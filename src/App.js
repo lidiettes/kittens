9f4de29a7dd8b452e7678da28096c8d02b1b3b72
lidiettes <hello@lidiettes.com>
2023-01-15 23:53:56 +0100

    call to api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-
-import { BrowserRouter } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import './App.css';
-import Router from './routes/Router';
 
-
+const GIPHY_API_KEY = "2cZkiFTqyiS79UdSapL6LHWlublpl7iy";
 
 function App() {
+
+  
+  const [catFact, setCatFact] = useState('');
+  const [catFactGiphy, setCatFactGiphy] = useState('');
+
+  const callGiphyAPI = (string) => {
+    fetch(`https://api.giphy.com/v1/gifs/search?q=${string}&api_key=${GIPHY_API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data[0].images.original.url);
+        setCatFactGiphy(data.data[0].images.original.url);
+      });
+  };
+
+  const callApi = () => {
+    fetch("https://catfact.ninja/fact")
+      .then((res) => res.json())
+      .then((data) => {
+        setCatFact(data.fact);
+        callGiphyAPI(data.fact.split(" ",3).join(' '))
+        console.log(data.fact);
+      });
+  }
+  useEffect(callApi, []);
+
   return (
-    <>
-      <BrowserRouter >
-        <Router />
-      </BrowserRouter>
-    </>
+
+    <div className='container'>
+      <p>{catFact}</p>
+      <img src={catFactGiphy} />
+      <button></button>
+    </div>
+
   );
 }
 
